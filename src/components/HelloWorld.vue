18004d838a4880addbb0248c671c6ceed3d9c28f
黑小马 <heixiaoma@gmail.com>
2024-04-08 15:59:32 +0800

    8

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,40 +6,36 @@ defineProps({
 })
 
 const showAlert = () => {
-  WebApp.showAlert(`Hello World! Current count is ${count}`)
+  WebApp.showAlert(`调用TG的结果`)
 }
-const count = ref(0)
 </script>
 
 <template>
-
   <button type="button" @click="showAlert">显示TG警告</button>
 
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <div class="container">
+    <iframe frameborder="0" scrolling="no" width="100%" height="100%" src="http://43.134.134.90:8888/mini-game/piano">
+    </iframe>
   </div>
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style scoped>
+/* 去除 iframe 边框 */
+iframe {
+  border: none;
+  width: 100vw;
+  height: 100vh;
+}
+
+/* 让 iframe 充满父容器 */
+.container {
+  margin: 0 auto;
+  position: relative;
+  width: 100vw;
+  height: 0;
+  padding-top: 56.25%; /* 16:9 比例 */
+}
 .read-the-docs {
   color: #888;
 }
